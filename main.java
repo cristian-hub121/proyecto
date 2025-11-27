ffcfbd67c9bdee38f7a9f4fcf511b6f750d56064
cristian-hub121 <cristiansamuelcor@gmail.com>
2025-11-27 05:34:46 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,12 +1,17 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pood2.empleado;
 
-public class main {
+package pood2.main;
+
+/**
+ *
+ * @author pc
+ */
+public class Main {
+
     public static void main(String[] args) {
-        Menu m = new Menu();
+       Menu m = new Menu();
         m.iniciar();
     }
 }
